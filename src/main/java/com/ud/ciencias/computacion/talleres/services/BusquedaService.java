6277a6e240ea37af5b7ee4ba09e8051bc638b6d0
David Sánchez Acero <159024664+4eyesBoi@users.noreply.github.com>
2026-02-15 18:00:13 -0500

    Add files via upload

---
+++ src/main/java/com/ud/ciencias/computacion/talleres/services/BusquedaService.java
@@ -0,0 +1,15 @@
+package com.ud.ciencias.computacion.talleres.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BusquedaService {
+
+    public void buscarServicioActivo() {
+        
+        System.out.println("buscando servicio activo...");
+        // Lógica para buscar servicios activos
+        
+    }
+
+}
---
+++ src/main/java/com/ud/ciencias/computacion/talleres/services/EliminarService.java
@@ -0,0 +1,5 @@
+package com.ud.ciencias.computacion.talleres.services;
+
+public class EliminarService {
+
+}
